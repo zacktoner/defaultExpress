f0ddf01311a8b152dc11edbaefe0c477c899c787
Zachary Toner <ztoner@10xpsolutions.com>
2016-01-04 18:31:21 -0800

    Adding mocha testing base

---
+++ test.js
@@ -0,0 +1,15 @@
+var request = require('supertest');
+var app = require('./app');
+
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
+describe('Requests to the root path', function() {
+  it('Returns a 200 status code', function(done) {
+    request(app)
+      .get('/')
+      .expect(200,done);
+  });
+});
